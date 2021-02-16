02599e8b72b4202bbfc07d4d04d9c4fdc3b22901
Sai Akshith V <akshith292@gmail.com>
2021-02-17 00:52:30 +0530

    added database connection and empty file

--- routes/welcomePage.js
+++ routes/welcomePage.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'software_project',
+	multipleStatements: true
+});
+con.connect(function(err) {
+	if (err) throw err;
+	console.log('MySQL Database Connected');
+});
 
 router.get('/welcome', (req, resp) => {
 	resp.render('welcomePage');
