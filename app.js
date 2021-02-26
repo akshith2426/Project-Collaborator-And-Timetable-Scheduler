13269fbe23ab70305bdb01b4b53fcda1181b7773
Sai Akshith V <akshith292@gmail.com>
2021-02-26 17:23:26 +0530

    added basic time-tables with buttons and bootstrap

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 //Routes
 app.use('/', require('./routes/welcomePage'));
+app.use('/FreeTimeScheduler', require('./routes/FreeTimeScheduler'));
 
 let port = process.env.PORT;
 if (port == null || port == '') {
---
+++ routes/FreeTimeScheduler.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'software_project',
+	multipleStatements: true
+});
+con.connect(function(err) {
+	if (err) throw err;
+	console.log('MySQL Database Connected');
+});
+
+router.get('/MTEB', (req, resp) => {
+	resp.render('MorTheoEvngLab');
+});
+router.get('/MLET', (req, resp) => {
+	resp.render('MorLabEvngTheo');
+});
+
+module.exports = router;
