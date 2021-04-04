86d93dfe4bc32997ff45b2e64f9d3fda22b6c1d5
Sampreeth2002 <61119309+Sampreeth2002@users.noreply.github.com>
2021-04-04 20:21:00 +0530

    added FreeSlots in database

--- routes/FreeTimeScheduler.js
+++ routes/FreeTimeScheduler.js
@@ -1,24 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 var con = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'software_project',
-	multipleStatements: true
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "software_project",
+  multipleStatements: true,
 });
-con.connect(function(err) {
-	if (err) throw err;
-	console.log('MySQL Database Connected');
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("MySQL Database Connected");
 });
 
-router.get('/MTEB', (req, resp) => {
-	resp.render('morTheoEvngLab');
+//Auth Logic
+
+//Batch Routes
+
+router.get("/MTEB", (req, resp) => {
+  resp.render("morTheoEvngLab");
 });
-router.get('/MLET', (req, resp) => {
-	resp.render('morLabEvngTheo');
+
+router.get("/MLET", (req, resp) => {
+  resp.render("morLabEvngTheo");
+});
+
+//Free Slots
+
+router.post("/freeSlots", (req, res) => {
+  var free = req.body.freeSlots;
+  var freeSlots = JSON.parse(free);
+  var inserQuery = "insert into timetablescheduler (freeSlot) values ?";
+  var query = mysql.format(inserQuery, [freeSlots.map((elm) => [elm])]);
+  console.log(query);
+  con.query(query, function (err, result) {
+    if (err) throw err;
+    res.send("Free Slots inserted in Database");
+  });
 });
 
 module.exports = router;
