ac422f8800758dccbbd3ec4a256c6b518a7ae921
Sai Akshith V <akshith292@gmail.com>
2021-05-05 10:10:18 +0530

    added login signup ui

--- software_project.sql
+++ software_project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2021 at 05:36 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Generation Time: Apr 06, 2021 at 01:06 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,41 +30,49 @@ SET time_zone = "+00:00";
 CREATE TABLE `timetablescheduler` (
   `freeSlotId` int(11) NOT NULL,
   `freeSlot` varchar(256) NOT NULL,
-  `id` int(11) NOT NULL,
-  `task` varchar(256) NOT NULL
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `timetablescheduler`
 --
 
-INSERT INTO `timetablescheduler` (`freeSlotId`, `freeSlot`, `id`, `task`) VALUES
-(1, '12', 6, ''),
-(2, '13', 6, 'Akshith'),
-(3, '15', 6, 'Jai balaya'),
-(4, '16', 6, ''),
-(5, '17', 6, ''),
-(6, '18', 6, ''),
-(7, '25', 6, ''),
-(8, '26', 6, ''),
-(9, '27', 6, ''),
-(10, '28', 6, ''),
-(11, '34', 6, 'sampreeth'),
-(12, '36', 6, 'Walking'),
-(13, '37', 6, 'Java DA'),
-(14, '38', 6, 'ALA DA'),
-(15, '41', 6, ''),
-(16, '42', 6, ''),
-(17, '43', 6, ''),
-(18, '44', 6, ''),
-(19, '45', 6, ''),
-(20, '47', 6, ''),
-(21, '48', 6, ''),
-(22, '51', 6, ''),
-(23, '54', 6, ''),
-(24, '55', 6, ''),
-(25, '56', 6, ''),
-(26, '58', 6, '');
+INSERT INTO `timetablescheduler` (`freeSlotId`, `freeSlot`, `id`) VALUES
+(332, '12', 6),
+(333, '13', 6),
+(334, '14', 6),
+(335, '15', 6),
+(336, '16', 6),
+(337, '17', 6),
+(338, '18', 6),
+(339, '22', 6),
+(340, '23', 6),
+(341, '24', 6),
+(342, '25', 6),
+(343, '26', 6),
+(344, '27', 6),
+(345, '28', 6),
+(346, '32', 6),
+(347, '33', 6),
+(348, '34', 6),
+(349, '35', 6),
+(350, '36', 6),
+(351, '37', 6),
+(352, '38', 6),
+(353, '42', 6),
+(354, '43', 6),
+(355, '44', 6),
+(356, '45', 6),
+(357, '46', 6),
+(358, '47', 6),
+(359, '48', 6),
+(360, '52', 6),
+(361, '53', 6),
+(362, '54', 6),
+(363, '55', 6),
+(364, '56', 6),
+(365, '57', 6),
+(366, '58', 6);
 
 -- --------------------------------------------------------
 
@@ -87,7 +95,7 @@ CREATE TABLE `users_table` (
 
 INSERT INTO `users_table` (`id`, `fullname`, `username`, `password`, `regno`, `batch`) VALUES
 (5, 'Akshith', 'akshith', 'hello123', '19BCE0095', 1),
-(6, 'sampreeth', 'sampreeth', 'sampreeth', '19BCE0865', 0);
+(6, 'sampreeth', 'sampreeth', 'sampu123', '19BCE0865', 0);
 
 --
 -- Indexes for dumped tables
@@ -97,8 +105,7 @@ INSERT INTO `users_table` (`id`, `fullname`, `username`, `password`, `regno`, `b
 -- Indexes for table `timetablescheduler`
 --
 ALTER TABLE `timetablescheduler`
-  ADD PRIMARY KEY (`freeSlotId`),
-  ADD KEY `id` (`id`);
+  ADD PRIMARY KEY (`freeSlotId`);
 
 --
 -- Indexes for table `users_table`
@@ -114,23 +121,13 @@ ALTER TABLE `users_table`
 -- AUTO_INCREMENT for table `timetablescheduler`
 --
 ALTER TABLE `timetablescheduler`
-  MODIFY `freeSlotId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `freeSlotId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=367;
 
 --
 -- AUTO_INCREMENT for table `users_table`
 --
 ALTER TABLE `users_table`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `timetablescheduler`
---
-ALTER TABLE `timetablescheduler`
-  ADD CONSTRAINT `timetablescheduler_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users_table` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
